834b142e9e40f08c77a41121043ad25b70c26c8a
Ibrohim2101 <70854852+Ibrohim2101@users.noreply.github.com>
2021-11-11 10:51:54 +0500

    add files

---
+++ 1/script.js
@@ -0,0 +1,45 @@
+var Ism = prompt("Ismingizni kiriting")
+if (!isNaN(Ism)) {
+    console.log("Siz ismingizni notog'ri kiritingiz");
+}
+else {
+    console.log("Sizning ismingiz " + Ism);
+}
+
+var Yosh = +prompt("Yoshingizni kiritin")
+if (!isNaN(Yosh)) {
+    console.log("Sizning yoshingiz " + Yosh);
+}
+else {
+    console.log("Siz sondan boshqa narsa kiritingiz");
+}
+var m1 = prompt("58+47=?")
+if (m1 == "105") {
+    console.log("Sizning javob tog'ri:  " + m1 + "                                  58+47=105");
+}
+else {
+    console.log("Sizning javob notog'ri:  " + m1 + "              To'g'ri javob:    58+47=105");
+}
+var m2 = prompt("75/3=?")
+if (m2 == 25) {
+    console.log("Sizning javob tog'ri:  " + m2 + "                                  75/3=25");
+}
+else {
+    console.log("Sizning javob notog'ri:  " + m2 + "              To'g'ri javob:    75/3=25");
+}
+var m3 = prompt("255/5=?")
+if (m3 == 25) {
+    console.log("Sizning javob tog'ri:  " + m3 + "                                  255/5=51");
+}
+else {
+    console.log("Sizning javob notog'ri:  " + m3 + "              To'g'ri javob:    255/5=51");
+}
+var m4 = prompt("99+1=?")
+if (m4 == 25) {
+    console.log("Sizning javob tog'ri:  " + m4 + "                                  99+1=100");
+}
+else {
+    console.log("Sizning javob notog'ri:  " + m4 + "              To'g'ri javob:    99+1=100");
+}
+
+
